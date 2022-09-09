d14322fd7d379965bedd82e8fc039370301b4e3b
Sacdees <104862958+Sacdees@users.noreply.github.com>
2022-09-09 01:15:14 -0400

    update

--- Earthquake_Challenge/static/jss/challenge_logic.js
+++ Earthquake_Challenge/static/jss/challenge_logic.js
@@ -30,11 +30,16 @@ let baseMaps = {
 
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
+let tectonicPlates = new L.LayerGroup();
+let majorEarthquakes = new L.LayerGroup();
 
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
-  "Earthquakes": allEarthquakes
+  "Earthquakes": allEarthquakes,
+  "Tectonic Plates": tectonicPlates,
+  "Major Earthquakes": majorEarthquakes
+
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -141,7 +146,8 @@ legend.onAdd = function() {
 
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json().then(() {
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
+
     
   });
 });
